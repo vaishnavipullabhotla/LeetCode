03d99148b2681679a2589fd5eb795850194a50a9
Vaishnavi Pullabhotla <165946035+vaishnavipullabhotla@users.noreply.github.com>
2025-07-18 01:08:44 +0530

    Time: 31 ms (56.91%), Space: 51.4 MB (83.41%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,10 +1,13 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
+        //Optimal
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<n-1;i++)
         {
+            if(i>0 && nums[i-1]==nums[i])
+            continue;
             int l=i+1,r=n-1;
             while(l<r)
             {
@@ -15,14 +18,17 @@ class Solution {
                 r--;
                 if(sum==0)
                 {
-                    List<Integer> list = Arrays.asList(nums[i],nums[l],nums[r]);
+                    while(l<r && nums[l]==nums[l+1])
+                    l++;
+                    while(l<r && nums[r-1]==nums[r])
+                    r--;
+                    list.add(Arrays.asList(nums[i],nums[l],nums[r]));
                     //this method is used to add directly numbers as a list
-                    set.add(list);
                     l++;
                     r--;
                 }
             }
         }
-        return new ArrayList<>(set);
+        return list;
     }
 }

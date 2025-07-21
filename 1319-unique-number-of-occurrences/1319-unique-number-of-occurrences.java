3f4d9e6fcd47815fb52906a6f053482be350891a
Vaishnavi Pullabhotla <165946035+vaishnavipullabhotla@users.noreply.github.com>
2025-07-22 00:06:49 +0530

    Time: 2 ms (98.05%), Space: 42 MB (55.95%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public boolean uniqueOccurrences(int[] arr) 
+    {
+        int n = arr.length;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i:arr)
+        {
+            if(hm.containsKey(i))//key
+            hm.put(i,hm.get(i)+1);
+            else
+            hm.put(i,1);
+        }
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i:hm.values())//values means counts
+        {
+            if(hs.contains(i))
+            return false;
+            else
+            hs.add(i);
+        }
+        return true;
+    }
+}

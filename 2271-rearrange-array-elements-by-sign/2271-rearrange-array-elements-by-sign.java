0cd80473b788ee4314e87bbb19891f53096c361f
Vaishnavi Pullabhotla <165946035+vaishnavipullabhotla@users.noreply.github.com>
2025-07-26 20:30:27 +0530

    Time: 3 ms (100%), Space: 81.8 MB (20.96%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,36 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n=nums.length;
-        // int i=0,j=i+1;
-        // while(i<j && j<n){
-        //     if((nums[i]>0 && nums[j]<0) || (nums[i]<0 && nums[j]>0)){
-        //         int temp=nums[i];
-        //         nums[i]=nums[j];
-        //         nums[j]=temp;
-        //     }
-        //     if(nums[i]>0 && nums[j]>0){
-        //         i++;
-        //         j++;
-        //     }
-        //     else
-        //     break;
-        // }
-        int pos[]=new int[n];
-        int neg[]=new int[n];
-        int cnt1=0,cnt2=0;
+        int res[]=new int[n];
+        int pos=0,neg=1;
         for(int i=0;i<n;i++){
-            if(nums[i]>0)
-            pos[cnt1++]=nums[i];
-            else
-            neg[cnt2++]=nums[i];
+            if(nums[i]>0){
+                res[pos]=nums[i];
+                pos=pos+2;
+            }
+            else{
+                res[neg]=nums[i];
+                neg=neg+2;
+            }
         }
-        int cnt=0;
-        cnt1=0;
-        cnt2=0;
-        for(int i=0;i<n;i=i+2){
-            nums[cnt++]=pos[cnt1++];
-            nums[cnt++]=neg[cnt2++];
-        }
-        return nums;
+        return res;
     }
 }

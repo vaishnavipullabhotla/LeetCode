ad454690348de777d281c1be7dad9fecaf540a61
Vaishnavi Pullabhotla <165946035+vaishnavipullabhotla@users.noreply.github.com>
2025-08-12 22:51:10 +0530

    Time: 0 ms (100%), Space: 40.3 MB (70.6%) - LeetHub

---
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -0,0 +1,14 @@
+class Solution 
+{
+    public int commonFactors(int a, int b) 
+    {
+        int min = Math.min(a,b);
+        int cnt = 0;
+        for(int i=1;i<=min;i++)
+        {
+            if(a%i == 0 && b%i == 0)
+            cnt++;
+        }
+        return cnt;
+    }
+}

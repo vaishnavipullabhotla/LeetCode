d4a8cd79a049bf116c69efcd6eb612b28a023715
Vaishnavi Pullabhotla <165946035+vaishnavipullabhotla@users.noreply.github.com>
2025-09-17 11:52:57 +0530

    Time: 5 ms (15.92%), Space: 40.2 MB (93.37%) - LeetHub

---
+++ 0856-consecutive-numbers-sum/0856-consecutive-numbers-sum.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int consecutiveNumbersSum(int n) 
+    {
+        int cnt = 0;
+        //formula: x = (num-(n(n-1)/2))/n
+        for(int i=1;i*(i+1)/2<=n;i++)
+        {
+            int numerator = (n-(i*(i-1)/2));
+            if(numerator % i == 0)
+            {
+                int x = numerator/i;
+                if(x > 0)
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
